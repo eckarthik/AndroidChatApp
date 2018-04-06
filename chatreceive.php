af13593422b4714ad91cb82b82ffed6eb972102d
Karthik <eckarthik39@gmail.com>
2018-04-06 20:20:34 +0530

    Updated chatreceive.php to provide last seen status

--- chatreceive.php
+++ chatreceive.php
@@ -8,12 +8,25 @@ mysqli_select_db($connection,$dbname) or die("Unable to select DB".mysqli_error(
 if(isset($_POST['name']))
 {
     $name = $_POST['name'];
+    $lastseentimestamp = $_POST['currentuseractivetimestamp'];
+    $query3 = mysqli_query($connection,"UPDATE chatusers SET lastseen='$lastseentimestamp' WHERE name='$name'");
+    if($query3)
+    {
+        $response["lastseenupdate"] = "Success";
+    }
+    else
+    {
+        $response["lastseenupdate"] = "Failed";
+    }
     $query = mysqli_query($connection,"SELECT * FROM chatmessages where name='$name' ORDER by timestamp DESC");
     $arrayresult = mysqli_fetch_array($query,MYSQLI_BOTH);
     $messageinDB = $arrayresult[1];
     $timestampinDB = $arrayresult[2];
     if($query)
     {
+        $query2 = mysqli_query($connection,"SELECT * FROM chatusers WHERE name='$name'");
+        $arrayresult2 = mysql_fetch_array($query,MYSQLI_BOTH);
+        $response["lastseen"] = $arrayresult2[3];
         $response["success"] = true;
         $response["message"] = $messageinDB;
         $response["timestamp"] = $timestampinDB;
