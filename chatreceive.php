4836214d4f36df8c59ba91dfdc11383f3db5eef9
Karthik <eckarthik39@gmail.com>
2018-04-06 17:09:51 +0530

    Added chatsend.php and chareceive.php

---
+++ chatreceive.php
@@ -0,0 +1,33 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "KartProj96$";
+$dbname = "chatapp";
+$connection = mysqli_connect($dbhost,$dbuser,$dbpass) or die ("Unable to connect to MySQL DB:".mysqli_error($connection));
+mysqli_select_db($connection,$dbname) or die("Unable to select DB".mysqli_error($connection));
+if(isset($_POST['name']) && isset($_POST['message']))
+{
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    $timestamp = $_POST['timestamp'];
+    $query = mysqli_query($connection,"SELECT * FROM chatmessages where name='$name' ORDER by timestamp DESC");
+    $arrayresult = mysqli_fetch_row($query,MYSQLI_BOTH);
+    $messageinDB = $arrayresult[1];
+    $timestampinDB = $arrayresult[2];
+    if(mysqli_num_rows($query)==1)
+    {
+        $response["success"] = true;
+        $response["message"] = $messageinDB;
+        $response["timestamp"] = $timestampinDB;
+    }
+    else
+    {
+        $response["success"] = false;
+    }
+    echo json_encode($response);
+}
+else
+{
+    echo "Required Data Missing";
+}
+?>
---
+++ chatsend.php
@@ -0,0 +1,28 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "KartProj96$";
+$dbname = "chatapp";
+$connection = mysqli_connect($dbhost,$dbuser,$dbpass) or die ("Unable to connect to MySQL DB:".mysqli_error($connection));
+mysqli_select_db($connection,$dbname) or die("Unable to select DB".mysqli_error($connection));
+if(isset($_POST['name']) && isset($_POST['message']))
+{
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    $timestamp = $_POST['timestamp'];
+    $query = mysqli_query($connection,"INSERT INTO chatmessages(name,message,timestamp) VALUES('$name','$messages','$timestamp')");
+    if($query)
+    {
+        $response["success"] = true;
+    }
+    else
+    {
+        $response["success"] = false;
+    }
+    echo json_encode($response);
+}
+else
+{
+    echo "Required Data Missing";
+}
+?>
