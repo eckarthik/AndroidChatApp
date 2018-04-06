b8d7f30cc19052b111222119cb5c4188083932e6
Karthik <eckarthik39@gmail.com>
2018-04-07 00:05:01 +0530

    Modified getAllMessages.php

--- getAllMessages.php
+++ getAllMessages.php
@@ -10,22 +10,29 @@ if(isset($_POST['name']) && isset($_POST['friendname']))
 {
     $name = $_POST['name'];
     $friendname = $_POST['friendname'];
-    $query = mysqli_query($connection,"SELECT message,timestamp FROM chatmessages WHERE name='$name' ORDER BY timestamp ASC");
+    $query = mysqli_query($connection,"SELECT name,message,timestamp FROM chatmessages WHERE fromname='$name' and name='$friendname'");
     $messages = array();
     while ($row=mysqli_fetch_row($query))
     {
-        $messages["$row[1]"] = array($row[0],$name);
+        if($row[0]==$name)
+        {
+            $messages["$row[1]"] = array($row[1],$name,$friendname);
+        }
+        else
+        {
+            $messages["$row[1]"] = array($row[1],$friendname,$name);
+        }
         
     }
      mysqli_free_result($query);
-    $query = mysqli_query($connection,"SELECT message,timestamp FROM chatmessages WHERE name='$friendname' ORDER BY timestamp ASC");
+    /*$query = mysqli_query($connection,"SELECT message,timestamp FROM chatmessages WHERE name='$friendname' ORDER BY timestamp ASC");
     while ($row=mysqli_fetch_row($query))
     {
-        $messages["$row[1]"] = array($row[0],$name);
+        $messages["$row[1]"] = array($row[0],$friendname);
         
     }
      mysqli_free_result($query);
-     $sorted = sort($messages);
+     $sorted = sort($messages);*/
      echo json_encode($messages);
 }
 else
