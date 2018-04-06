d6772df2ca8e4231de390d56339e4f8834a30b72
Karthik <eckarthik39@gmail.com>
2018-04-06 23:51:07 +0530

    Updated chatsend.php to add both fromname,toname to the database

--- chatsend.php
+++ chatsend.php
@@ -10,12 +10,13 @@ $dbpass = "KartProj96$";
 $dbname = "chatapp";
 $connection = mysqli_connect($dbhost,$dbuser,$dbpass) or die ("Unable to connect to MySQL DB:".mysqli_error($connection));
 mysqli_select_db($connection,$dbname) or die("Unable to select DB".mysqli_error($connection));
-if(isset($_POST['name']) && isset($_POST['message']))
+if(isset($_POST['toname']) && isset($_POST['message']) && isset($_POST['fromname']))
 {
-    $name = $_POST['name'];
+    $toname = $_POST['toname'];
+    $fromname = $_POST['fromname'];
     $message = attack_filter($connection,$_POST['message']);
     $timestamp = $_POST['timestamp'];
-    $query = mysqli_query($connection,"INSERT INTO chatmessages(name,message,timestamp) VALUES('$name','$message','$timestamp')");
+    $query = mysqli_query($connection,"INSERT INTO chatmessages(toname,fromname,message,timestamp) VALUES('$toname','$fromname','$message','$timestamp')");
     if($query)
     {
         $response["success"] = true;
